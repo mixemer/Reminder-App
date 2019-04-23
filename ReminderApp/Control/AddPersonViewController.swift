cdc9daf2611be27971d61f8df58becbeea295c22
mixemer <mehmetshin96@gmail.com>
2019-04-23 17:56:01 -0400

    add DatePicker to birthday text field

--- ReminderApp/Control/AddPersonViewController.swift
+++ ReminderApp/Control/AddPersonViewController.swift
@@ -13,13 +13,13 @@ class AddPersonViewController: UIViewController, UITextFieldDelegate {
     @IBOutlet weak var imgView: UIImageView!
     @IBOutlet weak var firstName: UITextField!
     @IBOutlet weak var lastName: UITextField!
-    @IBOutlet weak var titleField: UITextField!
+    @IBOutlet weak var groupField: UITextField!
     @IBOutlet weak var phoneNum: UITextField!
     @IBOutlet weak var emailField: UITextField!
     @IBOutlet weak var birthdayField: UITextField!
     @IBOutlet weak var notesField: UITextField!
     
-    var datePicker = UIDatePicker()
+    let datePicker = UIDatePicker()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -30,6 +30,10 @@ class AddPersonViewController: UIViewController, UITextFieldDelegate {
         NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillChange(notification:)), name: UIResponder.keyboardWillShowNotification, object: nil)
         NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillChange(notification:)), name: UIResponder.keyboardWillChangeFrameNotification, object: nil)
         NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillChange(notification:)), name: UIResponder.keyboardWillHideNotification, object: nil)
+        
+        
+        addDatePickerToBirthdayField()
+        
     }
     
     deinit {
@@ -37,6 +41,35 @@ class AddPersonViewController: UIViewController, UITextFieldDelegate {
         NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillChangeFrameNotification, object: nil)
         NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
     }
+    
+    func addDatePickerToBirthdayField() {
+        datePicker.datePickerMode = .date
+        
+        // ToolBar
+        let toolbar = UIToolbar()
+        toolbar.sizeToFit()
+        let doneButton = UIBarButtonItem(title: "Done", style: .plain, target: self, action: #selector(doneDatePicker))
+        let spaceButton = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.flexibleSpace, target: self, action: nil)
+        let cancelButton = UIBarButtonItem(title: "Cance", style: .plain, target: self, action: #selector(cancelDatePicker))
+        
+        toolbar.setItems([doneButton,spaceButton,cancelButton], animated: false)
+        
+        birthdayField.inputAccessoryView = toolbar
+        birthdayField.inputView = datePicker
+    }
+    
+    @objc func doneDatePicker() {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "dd/MM/yyyy"
+        birthdayField.text = formatter.string(from: datePicker.date)
+        self.view.endEditing(true)
+    }
+    
+    @objc func cancelDatePicker() {
+        self.view.endEditing(true)
+    }
+    
+    
     
     @objc func keyboardWillChange(notification: Notification) {
         guard let keyboardRect = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue else {
@@ -60,4 +93,6 @@ class AddPersonViewController: UIViewController, UITextFieldDelegate {
         hideKeyboard()
         return true
     }
+    
+    
 }
